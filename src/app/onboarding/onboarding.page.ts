69072a8c595c33c5f5b636618d508f806553d1f6
Aman Pawar <amanrpawar1210@gmail.com>
2025-03-08 14:15:53 +0530

    bootstrap added

--- src/app/onboarding/onboarding.page.ts
+++ src/app/onboarding/onboarding.page.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-onboarding',
   templateUrl: './onboarding.page.html',
   styleUrls: ['./onboarding.page.scss'],
+  standalone: false
 })
 export class OnboardingPage implements OnInit {
 
